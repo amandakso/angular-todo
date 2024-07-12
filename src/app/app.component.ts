843ca5eb1e337cfa252796cd8e0892e6a99c3ddf
Amanda So <soamanda95@gmail.com>
2024-07-12 11:19:04 -0700

    add logic to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,23 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'todo';
+  componentTitle = 'My To Do List';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter == 'done' ? item.done : !item.done
+    );
+  }
 }
